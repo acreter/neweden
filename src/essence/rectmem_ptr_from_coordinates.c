1e2b6050352613d3c58de585de7095221771666a
acreter@genesis <peter.markfelder@stud-mail.uni-wuerzburg.de>
2021-07-19 15:15:51 +0200

    Better coordinates -> pointer function

---
+++ src/essence/rectmem_ptr_from_coordinates.c
@@ -0,0 +1,23 @@
+/* Copyright (C) 2021 Peter Markfelder
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public
+ * License version 3 as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>. */
+
+#include <neweden/essence/rectangular_memory.h>
+
+char*
+rectmem_ptr_from_coordinates(
+		char* block,
+		size_t x,
+		size_t y
+) {
+	return block + y * rectmem_dimensions(block)->x +x + x;
+}
